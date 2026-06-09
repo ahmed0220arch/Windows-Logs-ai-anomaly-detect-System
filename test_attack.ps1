f2eaf3b351f4a228fe8e1cd58f48a4dc9f39ccab
Ahmed Dridi <ahmed0220arch@github.com>
2026-06-09 19:19:54 +0200

    Fix event IDs for test attack script to trigger Render redeploy

--- test_attack.ps1
+++ test_attack.ps1
@@ -25,7 +25,7 @@ Start-Sleep -Seconds 5
 
 Write-Host "[2/3] Initiating Print Spooler Exploit (PrintNightmare)..." -ForegroundColor Yellow
 1..5 | ForEach-Object { 
-    eventcreate /t ERROR /id 7031 /l system /d "The Print Spooler service terminated unexpectedly. It has done this 1 time(s). The following corrective action will be taken in 60000 milliseconds: Restart the service." | Out-Null
+    eventcreate /t ERROR /id 998 /l system /d "The Print Spooler service terminated unexpectedly. It has done this 1 time(s). The following corrective action will be taken in 60000 milliseconds: Restart the service." | Out-Null
     Start-Sleep -Milliseconds 500
 }
 Write-Host "  -> 5 Print Spooler exploit logs injected." -ForegroundColor Green
@@ -36,7 +36,7 @@ Start-Sleep -Seconds 5
 
 Write-Host "[3/3] Initiating SQL Injection Database Attack..." -ForegroundColor Yellow
 1..10 | ForEach-Object { 
-    eventcreate /t ERROR /id 3000 /l application /d "FATAL: Massive SQL Injection pattern detected in incoming HTTP request to backend database." | Out-Null
+    eventcreate /t ERROR /id 997 /l application /d "FATAL: Massive SQL Injection pattern detected in incoming HTTP request to backend database." | Out-Null
     Start-Sleep -Milliseconds 300
 }
 Write-Host "  -> 10 SQL Injection logs injected." -ForegroundColor Green
